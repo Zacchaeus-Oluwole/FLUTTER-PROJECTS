b3474eb5c308a12ee4165fae43ec11cfea1506e3
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-07-06 09:19:52 +0100

    ...

--- recipes/lib/recipe.dart
+++ recipes/lib/recipe.dart
@@ -1,12 +1,33 @@
 class Recipe {
   String label;
   String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
 
-  Recipe(this.label, this.imageUrl);
-  
+  Recipe(this.label, this.imageUrl, this.ingredients, this.servings);
+
   static List<Recipe> samples = [
-    Recipe('Chocolate', 'assets/chocolate.jpg'),
-    Recipe('Bread Rolls', 'assets/breadRolls.jpg'),
-    Recipe('Jollof Rice', 'assets/jollof.jpg')
+    Recipe('Chocolate', 'assets/chocolate.jpg',[
+      Ingredient(2, 'cup', 'Chocolate'),
+      Ingredient(0.5, 'cup', 'water')
+      ],1),
+    Recipe('Bread Rolls', 'assets/breadRolls.jpg',[
+      Ingredient(4, 'box', 'Bread'),
+      Ingredient(1, 'sachet', 'Butter')
+    ],5),
+    Recipe('Jollof Rice', 'assets/jollof.jpg',[
+      Ingredient(0.2, 'bag', 'Rice'),
+      Ingredient(8, 'cubes', 'Magi'),
+      Ingredient(1, 'bottle', 'Oil')
+    ],2)
   ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
+
 }
--- recipes/lib/recipe_detail.dart
+++ recipes/lib/recipe_detail.dart
@@ -36,7 +36,18 @@ class _RecipeDetailState extends State<RecipeDetail> {
                 color: blackColor,
                 fontSize: 15,
               ),
-            )
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index){
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text('${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+                ),
+                
+              ),
           ],
           
           ),
